77cfc442d595069648129fba24d6a45f9095cdcf
Mitch <mitchell.sturge@gmail.com>
2019-09-24 19:15:02 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import MoodIcon from '@material-ui/icons/Mood';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class MyForm extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       inputValue: ""
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if(this.state.inputValue !== prevState.inputValue) {
+//       console.log("State CHANGEROO")
+//     }    
+//   }
+
+//   handleChange = (event) => {
+//     this.setState({
+//       inputValue: event.target.value
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <label>
+//           Name:
+//           <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+//         </label>
+//         <p> {this.state.inputValue} </p>
+//       </form>
+//     )
+//   }
+// }
+
+class App extends Component {
+  constructor() {
+    super()
+  } 
+
+  render() {
+    return (
+      <div className="App">
+        <Button color="secondary">Click Me <MoodIcon color="primary" /></Button>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
